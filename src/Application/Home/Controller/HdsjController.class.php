2938bf396f986251752cbfbdb018eba0014ab50a
ZGX <1447905210@qq.com>
2015-07-29 17:05:29 +0800

    安全提交

---
+++ src/Application/Home/Controller/HdsjController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class HdsjController extends Controller {
+    //默认是跳转到社会实践
+    public function shsj(){
+        $this->show();
+    }
+    //志愿服务
+    public function zyfw(){
+        $this->show();
+    }
+}

fe09cd2d0811afe64a11d13977bf7713408cdd4e
zhangguixu <1447905210@qq.com>
2015-09-25 11:09:54 +0800

    1、首页的中部的修改

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -13,21 +13,26 @@ class IndexController extends Controller {
         $noticeorderby['noticeid'] = 'desc';
         $listNotice=$notice->where('noticeAuditId=1')->order($noticeorderby)->limit(6)->select();
 
-        //活动实践
-        $activitypractice=M('activitypractice');
-        $activitypracticeorderby['activitypracticeid']='desc';
-        $listActivity=$activitypractice->where('activitypracticeauditstatus=1')->order ( $activitypracticeorderby )->limit (6)->select ();
+//        //活动实践
+//        $activitypractice=M('activitypractice');
+//        $activitypracticeorderby['activitypracticeid']='desc';
+//        $listActivity=$activitypractice->where('activitypracticeauditstatus=1')->order ( $activitypracticeorderby )->limit (6)->select ();
+//
+//        //支部通知
+//        $apperance=M('branchapperance');
+//        $apperanceorderby['branchapperanceid']='desc';
+//        $listApperance=$apperance->where('branchapperanceacademyauditstatus=1')->order($apperanceorderby)->limit(6)->select();
 
-        //支部通知
-        $apperance=M('branchapperance');
-        $apperanceorderby['branchapperanceid']='desc';
-        $listApperance=$apperance->where('branchapperanceacademyauditstatus=1')->order($apperanceorderby)->limit(6)->select();
+        //下载专区
+        $download=M('download');
+        $downloadorderby['downloadid']='desc';
+        $listDownload=$download->order($downloadorderby)->limit(6)->select();
 
     	$this->assign('listWorkTendency',$listWorkTendency);
         $this->assign('listNotice',$listNotice);
-        $this->assign("listActivity",$listActivity);
-        $this->assign("listApperance",$listApperance);
-
+//        $this->assign("listActivity",$listActivity);
+//        $this->assign("listApperance",$listApperance);
+        $this->assign('listDownload',$listDownload);
         $this->display();
     }
     
